2f6053c1850e4b6e3abc9bdb341a4e10ae07c248
OleksandrNibyt <onibyt@mail.sfsu.edu>
2019-12-19 16:57:08 -0800

    Add files via upload

---
+++ JavaList/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            int value = scan.nextInt();
+            list.add(value);
+        }
+
+
+        int Q = scan.nextInt();
+        for (int i = 0; i < Q; i++) {
+            String action = scan.next();
+            if (action.equals("Insert")) {
+                int index = scan.nextInt();
+                int value = scan.nextInt();
+                list.add(index, value);
+            } else
+                 {
+                int index = scan.nextInt();
+                list.remove(index);
+            }
+        }
+        scan.close();
+
+        for (Integer num : list) {
+            System.out.print(num + " ");
+        }
+    }
+}
