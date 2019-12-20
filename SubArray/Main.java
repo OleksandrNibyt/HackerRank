32ca40d0e4e9306d7e60e8d00cf429d33c5a7c0f
OleksandrNibyt <onibyt@mail.sfsu.edu>
2019-12-20 11:37:01 -0800

    Add files via upload

---
+++ SubArray/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner scan = new Scanner(System.in);
+        int size     = scan.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scan.nextInt();
+        }
+        scan.close();
+
+        System.out.println(negativeSubarrays(array));
+    }
+
+
+
+    private static int negativeSubarrays(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            int sum = 0;
+            for (int j = i; j < array.length; j++) {
+                sum += array[j];
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+}
+
