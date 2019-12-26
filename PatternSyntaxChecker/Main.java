9156683660aba44e2e204443a7d67e5577f59b7e
OleksandrNibyt <onibyt@mail.sfsu.edu>
2019-12-25 16:01:48 -0800

    Add files via upload

---
+++ PatternSyntaxChecker/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+
+
+
+
+            //Write your code
+
+
+
+        }
+    }
+}
